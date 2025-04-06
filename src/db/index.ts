90e570d5214a8004fdf5806073eb8f505e099159
prakhar17072004 <pmankur2004@gmail.com>
2025-04-06 17:12:50 +0530

    schema is updated

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,9 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle } from "drizzle-orm/node-postgres";
+import pg from "pg"
 
-const db = drizzle(process.env.DATABASE_URL!);
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // Required for Neon
+});
+
+export const db = drizzle(pool);
